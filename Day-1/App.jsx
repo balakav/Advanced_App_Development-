47135014d40a5bb069b700cd84d3df213a38f9ab
BALAKAVI B <118712303+balakav@users.noreply.github.com>
2024-01-23 14:53:47 +0530

    Add files via upload

---
+++ Day-1/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import First from './Components/First'
+import Home from './Components/Home'
+
+const App = () => {
+  return (
+    <div><Home></Home></div>
+  )
+}
+
+export default App
